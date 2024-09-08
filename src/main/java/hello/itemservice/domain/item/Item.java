000262bca0919b1b5dbaba25244b5bf4364b0ed8
Oh <osm723@gmail.com>
2024-09-08 20:27:38 +0900

    스프링 MVC 2편 - 백엔드 웹 개발 활용 기술 섹션 6. 검증2 - Bean Validation 정리

---
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -0,0 +1,36 @@
+package hello.itemservice.domain.item;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.ScriptAssert;
+
+@Data
+//@ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "전체 가격은 10000원 이상이어야 합니다.")
+public class Item {
+
+    //@NotNull(groups = UpdateCheck.class)
+    private Long id;
+
+    //@NotBlank(groups = {SaveCheck.class, UpdateCheck.class})
+    private String itemName;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Range(min = 1000, max = 1_000_000, groups = {SaveCheck.class, UpdateCheck.class})
+    private Integer price;
+
+    //@NotNull(groups = {SaveCheck.class, UpdateCheck.class})
+    //@Max(value = 9999, groups = SaveCheck.class)
+    private Integer quantity;
+
+    public Item() {
+    }
+
+    public Item(String itemName, Integer price, Integer quantity) {
+        this.itemName = itemName;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
